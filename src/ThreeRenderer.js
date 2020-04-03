b313f60521025cbe9c5ce0b8d6df6529c272b09c
Simone Carella <simonecarella@gmail.com>
2020-04-03 12:09:32 +0200

    lerp nodes (webgl)

--- src/ThreeRenderer.js
+++ src/ThreeRenderer.js
@@ -2,7 +2,7 @@ import React, { useCallback, useEffect, useRef, useMemo, useState } from "react"
 import { useCanvas, useFrame } from "react-three-fiber"
 import * as THREE from "three"
 import { CircleBufferGeometry } from "three"
-import gsap from "gsap"
+import lerp from "lerp"
 
 const dummy = new THREE.Object3D()
 
@@ -19,37 +19,60 @@ export default function ThreeRenderer({ nodes, onClickCanvas, simulation }) {
     return new CircleBufferGeometry(32, 128)
   }, [])
 
-  // When we're here it's loaded, now compute vertex normals
+  // compute vertex normals
   useMemo(() => {
     geometry.computeVertexNormals()
     geometry.scale(0.5, 0.5, 0.5)
   }, [geometry])
+
+  const animations = useRef({})
+
+  useEffect(() => {}, [nodes])
+
   // Compute per-frame instance positions
   const ref = useRef()
-  useFrame(state => {
+  let t = 0
+  useFrame((state, delta) => {
     // ref.current.rotation.x = Math.sin(time / 4)
     // ref.current.rotation.y = Math.sin(time / 2)
     let x = 0
     let y = 0
-
-    const nodes = simulation.nodes()
+    t += 0.01
+    //const nodes = simulation.nodes()
 
     for (let i = 0; i < 1000; i++) {
       if (nodes[i] !== undefined) {
         const d = Object.assign({}, nodes[i], {})
 
-        x = (d.x+d.vx) - window.innerWidth / 2
-        y = (d.y+d.vy) - window.innerHeight / 2
+        x = d.x - window.innerWidth / 2
+        y = d.y - window.innerHeight / 2
+
+
+        if (!animations.current[d.id]) {
+          animations.current[d.id] = {
+            x: d.cx-window.innerWidth/2,
+            y: d.cy-window.innerHeight/2,
+            scale: 0.5
+          }
+        }
+
+        const anim = animations.current[d.id]
+        anim.x = lerp(anim.x, x, delta*2)
+        anim.y = lerp(anim.y, y, delta*2)
+
+        // if (anim.next.x < x) anim.next.x = lerp(anim.prev.x, x, t)
+        // if (anim.next.y < y) anim.next.y = lerp(anim.prev.y, y, t)
 
         // gsap.to(d, {
         //     x,
         //     y,
         //     duration: 0.25,
         //     onUpdate: () => {
-        //         
+        //
         //     },
         // })
-        dummy.position.set(x, y, 0)
+
+        dummy.position.set(anim.x, anim.y, 0)
         dummy.scale.set((d.r / 32) * 2, (d.r / 32) * 2, (d.r / 32) * 2)
         dummy.updateMatrix()
       }
